f8262bac3f37e7705e511032f0be38a8f926cad1
nikola-s6 <nikola.stojilkovic6@gmail.com>
2023-09-22 12:10:12 +0200

    db setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import typeorm from './config/typeorm';
+import { DatabaseModule } from './database.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      load: [typeorm],
+    }),
+    DatabaseModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
